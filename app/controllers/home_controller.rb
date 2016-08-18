b0bf6e2b7bc8f3a117a99e10eef702bb48191668
fifiteen82726 <fifiteen82726@gmail.com>
2016-08-19 01:42:40 +0800

    post時quote出問題

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,22 @@ class HomeController < ApplicationController
 
 
 	def transform
-		return "123"
+		# binding.pry
+		a = params["send"]
+		b = a.tr('/', '')
+		# input = %q(a)
+		# binding.pry
+		page = Nokogiri::HTML(b)   
+		img = page.css('img')[0]
+		src =  ""
+		alt =  ""
+		class_atr = ""
+		src =  img['src']
+		alt =  img['alt']
+		class_atr = img['class']
+		result = "<%= image_tag(\'" + src + '\', :class => \'' + class_atr + '\', :alt => \'' + alt + '\')%>'
+		p result
+		binding.pry
 	end
 
 end
