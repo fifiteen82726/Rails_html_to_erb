1a7215c995dd3ef8dacdba680666d04f08f79d9f
fifiteen82726 <fifiteen82726@gmail.com>
2017-01-01 22:03:05 +0800

    排版

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,59 +2,46 @@ require 'nokogiri'
 
 class HomeController < ApplicationController
 	def index
-		
+
 	end
 
 	def transform
-		# binding.pry
 		a = params["send"]
-		page = Nokogiri::HTML(a) 
-		img = page.css('img')[0] 
-		src =  "" 
+		page = Nokogiri::HTML(a)
+		img = page.css('img')[0]
+		src =  ""
 		alt =  ""
 		class_atr = ""
 		src =  img['src'] if img['src'].present?
 		alt =  img['alt'] if img['alt'].present?
-		class_atr = img['class'] if img['class'].present? 
+		class_atr = img['class'] if img['class'].present?
 		result = "<%= image_tag(\'" + src + '\', :class => \'' + class_atr + '\', :alt => \'' + alt + '\')%>'
-		render :json => {"result" => result } 
+		render :json => {"result" => result }
 	end
 
 
 	def link
 
-		
+
 	end
 
 
 	def form
-
 		a = params["send"]
-		
-		page = Nokogiri::HTML(a) 
+		page = Nokogiri::HTML(a)
 		form = page.css('form')[0]
-		
-
 		class_atr = ""
 		class_atr = form['class'] if form['class'].present?
-		
-
 		action = ""
 		action = form['action'] if form['action'].present?
-		
 		method = ""
 		method = form['method'] if form['method'].present?
-		
-		form_tag = "<%= form_tag(\'" + action + "\', method: \'" + method+  "\'" + ":class => \'" + class_atr+ "\' ) do %>"
-		
-
-
-		render :json => {"result" => form_tag } 
-
+		form_tag = "<%= form_tag(\'" + action + "\', method: \'" + method+  "\'" + ":class => \'" + class_atr+ "\' ) do |f| %>"
+		render :json => {"result" => form_tag }
 	end
 
 	def input
-		
+
 	end
 
 end
