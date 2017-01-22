dec2c88ab9b4d3313e61296467e59833548741f8
Coda Chang <fifiteen82726@gmail.com>
2017-01-22 23:32:14 +0800

    feature multiple column

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,21 +8,24 @@ class HomeController < ApplicationController
 	def transform
 		a = params["send"]
 		page = Nokogiri::HTML(a)
-		img = page.css('img')[0]
-		src =  ""
-		alt =  ""
-		class_atr = ""
-		src =  img['src'] if img['src'].present?
-		alt =  img['alt'] if img['alt'].present?
-		class_atr = img['class'] if img['class'].present?
-		result = "<%= image_tag(\'" + src + '\', :class => \'' + class_atr + '\', :alt => \'' + alt + '\')%>'
-		render :json => {"result" => result }
+		result_attr = {}
+		img_node = page.css('img').first
+    img_attributes = img_node.attributes.values # list of image attributes
+    	# e.g., to output key-value pairs:
+    img_attributes.each do |attr|
+      result_attr[attr.name] =  attr.value
+    end
+    result = "<%= image_tag(\'" + result_attr['src'] + "'"
+    result_attr.delete('src')
+    result_attr.each_with_index do |(k, v), index|
+    	result = result + ( ", #{k}: \'#{v}\'")
+    end
+    result = result + " %>"
+    render :json => {"result" => result }
 	end
 
 
 	def link
-
-
 	end
 
 
@@ -41,7 +44,6 @@ class HomeController < ApplicationController
 	end
 
 	def input
-
 	end
 
 end
